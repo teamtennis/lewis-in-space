53d43291fe174d3404cb7d948f7427fa53ae065d
Sarah Flynn <sarah.flynn@icloud.com>
2018-08-15 17:10:57 -0700

    progress on css for event page

--- js/services/api.js
+++ js/services/api.js
@@ -34,8 +34,6 @@ let storyEvents = [{
     img: '../../img/event1-placeholder.jpg',  
     choice0a: 'You flee into a nearby asteroid field where you hope you can outmaneuver or hide from the slime pirates.',
     choice0b: 'Remembering your promise to always come home to Princess Megan Organa, and knowing there is very little hope of escaping with the fuel you have left, you make a difficult decision: you hail the pirate ship and tell them about a nearby cruise ship full of rich creatures they could raid instead.',
-    buttonText0a: 'Choice A',
-    buttonText0b: 'Choice B',
     choice0aResult: 'The slime pirated followed you into the asteroid field. During the chase, the slime pirates\' ship sustained damage in an in an asteroid collision and turned back. As you passed out of the asteroid field, you ran out of fuel and drifted through space until a passing cargo ship picked your ship up.',
     choice0bResult: 'The slime pirates thank you for telling them about the cruise ship, but they want to take your ship and turn you into a slime dog anyway. Lucky for you, the cruise ship changes course and the pirates only have time to raid one of you. They chase the cruise ship, leaving you with enough fuel to continue your journey. You convince yourself that the cruise ship\'s security team will prevent the slime pirates from hurting any passengers.'
 }, {
@@ -44,8 +42,6 @@ let storyEvents = [{
     img: '../../img/event2-placeholder.jpg',  
     choice1a: 'You risk your life, ship, and use up most of the snack fuel you need to continue your travels in an attempt to prevent a cat-aststrophe. You change your heading to intercept the asteroid and plan to use your ship to nudge it onto a different course',
     choice1b: 'You have no weapons, a ship barely big enough for a dog, and have limited snack fuel. Even if you could get to the asteroid in time, you could easily run out of fuel and never make it home to Planet Alchemy where you have friends and family waiting for you. With some reluctance, you continue on your journey as if you never heard the distress signal',
-    buttonText1a: 'Choice A',
-    buttonText1b: 'Choice B',
     choice1aResult: 'Your outrageous, brave plan to save Furtuna9 worked! A small nudge from your ship changed the course of the asteroid enough that it missed the cat planet. Unfortunately, you\'ve used up most of your snack fuel and are at risk of becoming stranded in space. You have to make a stop and take some risks to get more fuel.',
     choice1bResult: 'By the time the asteroid strikes Furtuna9, you are about to cross out of cat-space with plenty of snack fuel to continue your journey. You can\'t see the impact and the planet is just a dot in the distance. You put it out of your mind, but you\'ve changed.'
 }, {
@@ -54,8 +50,6 @@ let storyEvents = [{
     img: '../../img/event3-placeholder.jpg',  
     choice2a: 'Fluffy is one of your best friends, and it breaks your heart to see her so frail and weak. You leave her with all of the snacks you can spare to help her heal and get back on her big feet.',
     choice2b: 'Everyone needs a little help sometimes so you give Fluffy just enough snacks fuel a one way trip to the nearest space station. She can get a job and earn her own way there. Give a Wampa a snack, feed her for a day, but teach a Wampa to get snacks, she feeds herself without taking from her friends.',
-    buttonText2a: 'Choice A',
-    buttonText2b: 'Choice B',
     choice2aResult: 'You fly away from Algid after a long, pleasant visit during which you and Fluffy always stayed an appropriate distance apart. You are a little concerned about your own snack fuel levels, but you feel good about helping your friend and know she would have done the same for you.',
     choice2bResult: 'You fly away from Algid after a short, pleasant visit. You were clear about your boundaries, and that\'s important in a friendship, right? You hope Fluffy does the right thing, using those snacks to find work rather than binge eating them.'
 }];
