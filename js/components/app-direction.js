7a96b73d7beac772f2195be4a4acdbc8264ec68e
Sophie Graham <sophiergraham1@gmail.com>
2018-08-16 11:58:56 -0700

    added grid

--- js/components/app-direction.js
+++ js/components/app-direction.js
@@ -4,13 +4,13 @@ import api from '../services/api.js';
 
 let template = function(user) {
     return html`   
-        <div class="direction-screen direction-grid">
-            <section>
-                <img src="${user.userShip}" alt="rocket" width="40%">          
-                <p class="direction-text">Which direction will Lewis choose to fly? Click an arrow.</p>
-                <button type="reset" class="reset-path">Reset path history</button>
-            </section>
+        <div class="direction-screen">
 
+            <img src="${user.userShip}" alt="rocket" width="40%"> 
+                     
+            <p class="direction-text">Which direction will Lewis choose to fly? Click an arrow.</p>
+            <button type="reset" class="reset-path">Reset path history</button>
+    
             <a href="event.html" class="left-arrow">
             <img src="img/left-arrow.png" width="20%" />   
             
