35fbd7b3208e6dd4c393c33c819afa1dbed94e36
Mike <mbennettillustration@gmail.com>
2018-08-16 17:39:09 -0700

    buttons working again

--- js/components/app-about.js
+++ js/components/app-about.js
@@ -2,11 +2,45 @@ import html from '../html.js';
 //import api from '../services/api.js';
 
 let template = function() {
-    return html`   
+    return html` 
+    <section class="about-crew">
+    <div id="sarah" class="crew">
+            <img src="img/sarah-about.png" alt="sarah-image">
+            <p>Favorite Dog: Goldendoodle <br>
+                Favorite Sci Fi Series: Battlestar Galactica <br>
+                Fun Fact: Tone Deaf Whistler.
+            </p>
+        </div>
+        
+        <div id="mike" class="crew">
+            <img src="img/mike-about.png" alt="mike-image">
+            <p>Favorite Dog: Dachshund <br>
+                Favorite Sci Fi Series: Futurama <br>
+                Fun Fact: Can Whistle.<br>
+            </p>
+        </div>
+        
+        <div id="sophie" class="crew">
+            <img src="img/sophie-about.png" alt="sophie-image">
+            <p>Favorite Dog: Air Bud <br>
+                Favorite Sci Fi Series: Star Trek Next Gen <br>
+                Fun Fact: Can't whistle. <br>
+            </p>
+        </div>
+        <div id="alex" class="crew">
+            <img src="img/alex-about.png" alt="alex-image">
+            <p>Favorite Dog: His boxer, Maude<br>
+                Favorite Sci Fi Series: Star Wars <br>
+                Fun Fact: Once traveled to Whistler, BC.
+            </p>
+        </div>
+        </section>  
         <section>                    
             <a href="choose-ship.html" class="play-again">
                 <img src="img/play-again.png" width="20%"/> 
         </section>
+
+        <a class="start-over" href="choose-ship.html"></a> 
     `;
 };
 
